67512c0829731713c9eb8c792f751b89855bd1bf
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-30 08:54:40 -0300

    Feat: Atividade 01 , finalizada

---
+++ Atividade_01/Atividade_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+double[] nota = new double[3];
+double soma = 0;
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Digite a {i + 1}° nota:");
+    nota[i] = double.Parse(Console.ReadLine());
+    soma = soma += nota[i];
+}
+double media = soma / nota.Length;
+Console.WriteLine($"O valor da sua média de notas é: {media}");
+
+
